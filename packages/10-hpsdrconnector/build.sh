6e1cf1a932d54a107a720bc0582fd9e35d86aaed
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-11-28 20:42:47 +0100

    remove x86 protection

--- packages/10-hpsdrconnector/build.sh
+++ packages/10-hpsdrconnector/build.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ $(uname -m) == "x86_64" ]]; then
-    BRANCH_ARG=""
-    if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
-        BRANCH_ARG="-b ${RELEASE_BRANCH}"
-    fi
-
-    git clone --depth 1 ${BRANCH_ARG} https://github.com/openwebrx/hpsdrconnector
-    pushd hpsdrconnector
-    if [[ ! -z ${BUILD_NUMBER:-} ]]; then
-        GBP_ARGS="--debian-branch=master --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
-        gbp dch ${GBP_ARGS}
-    fi
-    debuild -us -uc
-    popd
+BRANCH_ARG=""
+if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+    BRANCH_ARG="-b ${RELEASE_BRANCH}"
 fi
+
+git clone --depth 1 ${BRANCH_ARG} https://github.com/openwebrx/hpsdrconnector
+pushd hpsdrconnector
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+    GBP_ARGS="--debian-branch=master --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+    gbp dch ${GBP_ARGS}
+fi
+debuild -us -uc
+popd
