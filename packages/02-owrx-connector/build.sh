2b4bb264176f03b43aa0d4512ff5f75d7a784085
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-12-01 16:58:38 +0100

    implement debhelper build for owrx-connector

--- packages/02-owrx-connector/build.sh
+++ packages/02-owrx-connector/build.sh
@@ -7,10 +7,9 @@ if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
 fi
 git clone --depth 1 ${BRANCH_ARG} https://github.com/jketterl/owrx_connector.git
 pushd owrx_connector
-mkdir build
-pushd build
-cmake ..
-cpack
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+  GBP_ARGS="--debian-branch=develop --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  gbp dch ${GBP_ARGS}
+fi
+debuild -us -uc
 popd
-popd
-mv owrx_connector/build/*.deb .
