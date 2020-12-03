9458c75944583747e28f22914edf758803a546ce
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-12-03 23:34:12 +0100

    fix branches

--- packages/20-aprs-symbols/build.sh
+++ packages/20-aprs-symbols/build.sh
@@ -3,13 +3,14 @@ set -euo pipefail
 
 BRANCH_ARG=""
 if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
-    BRANCH_ARG="-b ${RELEASE_BRANCH}"
+    BRANCH="${RELEASE_BRANCH}"
+else
+    BRANCH="-b debian/sid"
 fi
-git clone --depth 1 ${BRANCH_ARG} https://github.com/jketterl/aprs-symbols-debian.git
+git clone --depth 1 -b ${BRANCH} https://github.com/jketterl/aprs-symbols-debian.git
 pushd aprs-symbols-debian
-git checkout debian/sid
 if [[ ! -z ${BUILD_NUMBER:-} ]]; then
-  GBP_ARGS="--debian-branch=debian/sid --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
   gbp dch ${GBP_ARGS}
 fi
 debuild -us -uc
