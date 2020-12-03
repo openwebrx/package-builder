45048c6e108676f7ece50fd1f0b451da676e8de7
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-12-03 23:55:33 +0100

    checkout all

--- packages/20-aprs-symbols/build.sh
+++ packages/20-aprs-symbols/build.sh
@@ -6,7 +6,7 @@ if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
 else
     BRANCH="debian/sid"
 fi
-git clone --depth 1 -b ${BRANCH} https://github.com/jketterl/aprs-symbols-debian.git
+git clone -b ${BRANCH} https://github.com/jketterl/aprs-symbols-debian.git
 pushd aprs-symbols-debian
 if [[ ! -z ${BUILD_NUMBER:-} ]]; then
   GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
