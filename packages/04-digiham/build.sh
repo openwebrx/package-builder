23bda724fba83974c788f2ab2b3732015f116d12
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-12-03 21:19:19 +0100

    switch digiham to debhelper

--- packages/04-digiham/build.sh
+++ packages/04-digiham/build.sh
@@ -7,10 +7,9 @@ if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
 fi
 git clone --depth 1 ${BRANCH_ARG} https://github.com/jketterl/digiham.git
 pushd digiham
-mkdir build
-pushd build
-cmake ..
-cpack
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+  GBP_ARGS="--debian-branch=develop --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  gbp dch ${GBP_ARGS}
+fi
+debuild -us -uc
 popd
-popd
-mv digiham/build/*.deb .
