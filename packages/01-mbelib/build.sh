a361200dcd0d6fd46b3f6478904659b70e45f52a
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-12-04 20:25:43 +0100

    switch to gbp for mbelib

--- packages/01-mbelib/build.sh
+++ packages/01-mbelib/build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git clone --depth 1 https://github.com/szechyjs/mbelib.git
+if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+    source /etc/os-release
+    BRANCH="${ID}/${VERSION_CODENAME}"
+else
+    BRANCH="experimental"
+fi
+git clone -b ${BRANCH} https://github.com/jketterl/mbelib.git
 pushd mbelib
 if [[ ! -z ${BUILD_NUMBER:-} ]]; then
-  GBP_ARGS="--debian-branch=master --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
   gbp dch ${GBP_ARGS}
+  git add debian/changelog
+  git commit -m "snapshot changelog"
 fi
-debuild -us -uc
+gbp buildpackage --git-debian-branch=$BRANCH -us -uc
 popd
