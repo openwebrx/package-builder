4f37d1af0de7555370f0afcf3628d9b7a5a4c65c
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-01-09 23:56:47 +0100

    add build number / snapshot builds

--- packages/06-m17/build.sh
+++ packages/06-m17/build.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git clone -b debian https://github.com/jketterl/m17-cxx-demod.git
+if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+    source /etc/os-release
+    BRANCH="${ID}/${VERSION_CODENAME}"
+else
+    BRANCH="debian"
+fi
+git clone -b ${BRANCH} https://github.com/jketterl/m17-cxx-demod.git
 pushd m17-cxx-demod
-gbp buildpackage --git-debian-branch=debian
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+  GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  gbp dch ${GBP_ARGS}
+  git add debian/changelog
+  git commit -m "snapshot changelog"
+fi
+gbp buildpackage --git-debian-branch=${BRANCH} -us -uc
 popd
