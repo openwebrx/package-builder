5acd68e389d9cfcaf5436a85da0b7912f886d575
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-05-30 16:18:20 +0200

    add codecserver build

---
+++ packages/03-codecserver/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+BRANCH_ARG=""
+if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+    BRANCH_ARG="-b ${RELEASE_BRANCH}"
+fi
+git clone --depth 1 ${BRANCH_ARG} https://github.com/jketterl/codecserver.git
+pushd codecserver
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+  GBP_ARGS="--debian-branch=develop --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  gbp dch ${GBP_ARGS}
+fi
+debuild -us -uc
+popd
