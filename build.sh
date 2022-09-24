855a4ebb5eddd32aad348d124473cbfe9b01799c
Jakob Ketterl <jakob.ketterl@gmx.de>
2022-09-24 17:20:26 +0200

    fix tags

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ for DIST in `cat dists/$ARCH`; do
     docker build --pull --build-arg BASE=${DIST} -f docker/Dockerfile-stable-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${STABLE_TAG} .
     docker push 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${STABLE_TAG}
 
-    docker build --pull --build-arg BASE=${STABLE_TAG} -f docker/Dockerfile-latest-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${LATEST_TAG} .
+    docker build --pull --build-arg STABLE_TAG=${STABLE_TAG} -f docker/Dockerfile-latest-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${LATEST_TAG} .
     docker push 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${LATEST_TAG}
 done
