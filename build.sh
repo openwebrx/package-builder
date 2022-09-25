6820db8f03874a79f32531c1f9deb9853d9a3501
Jakob Ketterl <jakob.ketterl@gmx.de>
2022-09-25 02:30:53 +0200

    introduce staging repository

--- build.sh
+++ build.sh
@@ -5,12 +5,15 @@ ARCH=$(uname -m)
 
 for DIST in `cat dists/$ARCH`; do
     TAG=${DIST//[:]/_}_${ARCH}
-    STABLE_TAG=${TAG}_stable
+    BASE_TAG=${TAG}_base
+    STAGING_TAG=${TAG}_staging
     LATEST_TAG=${TAG}_latest
 
-    docker build --pull --build-arg BASE=${DIST} -f docker/Dockerfile-stable-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${STABLE_TAG} .
-    docker push 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${STABLE_TAG}
+    docker build --pull --build-arg BASE=${DIST} -f docker/Dockerfile-base-debian -t package-builder:${BASE_TAG} .
 
-    docker build --pull --build-arg STABLE_TAG=${STABLE_TAG} -f docker/Dockerfile-latest-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${LATEST_TAG} .
+    docker build --build-arg BASE_TAG=${BASE_TAG} -f docker/Dockerfile-staging-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${STAGING_TAG} .
+    docker push 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${STAGING_TAG}
+
+    docker build --build-arg BASE_TAG=${BASE_TAG} -f docker/Dockerfile-latest-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${LATEST_TAG} .
     docker push 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${LATEST_TAG}
 done
