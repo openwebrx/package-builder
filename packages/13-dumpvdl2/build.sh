917efe1b8d752dfac86e33768a7c6b08ca65efea
Jakob Ketterl <jakob.ketterl@gmx.de>
2023-08-27 02:21:19 +0200

    add dumpvdl2 build

---
+++ packages/13-dumpvdl2/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+    source /etc/os-release
+    BRANCH="${ID}/${VERSION_CODENAME}"
+else
+    BRANCH="debian/sid"
+fi
+git clone -b ${BRANCH} https://github.com/openwebrx/dumpvdl2-debian
+pushd dumpvdl2-debian
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+  GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  gbp dch ${GBP_ARGS}
+  git add debian/changelog
+  git commit -m "snapshot changelog"
+fi
+gbp buildpackage --git-debian-branch=${BRANCH} --git-submodules -us -uc
+popd
