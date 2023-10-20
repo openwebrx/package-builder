f49256b8c0d6de52c1e7f01519bbc8afdaafd9cf
Jakob Ketterl <jakob.ketterl@gmx.de>
2023-10-21 01:48:27 +0200

    add build for airspy

---
+++ packages/34-pkg-airspy-host/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+    source /etc/os-release
+    BRANCH="${ID}/${VERSION_CODENAME}"
+else
+    BRANCH="debian/sid"
+fi
+git clone -b ${BRANCH} https://github.com/openwebrx/pkg-airspy-host
+wget -O airspyone-host_1.0.10.orig.tar.gz https://github.com/airspy/airspyone_host/archive/refs/tags/v1.0.10.tar.gz
+pushd pkg-airspy-host
+tar xvfz ../airspyone-host_1.0.10.orig.tar.gz --strip-components=1
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+  GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  gbp dch ${GBP_ARGS}
+  git add debian/changelog
+  git commit -m "snapshot changelog"
+fi
+gbp buildpackage --git-debian-branch=${BRANCH} --git-submodules -us -uc
+popd
