adb141c31296e91e6652ce8f049455d72d40a7ef
Jakob Ketterl <jakob.ketterl@gmx.de>
2023-10-29 17:39:29 +0100

    add packaging script for dream

---
+++ packages/14-dream/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+    source /etc/os-release
+    BRANCH="${ID}/${VERSION_CODENAME}"
+else
+    BRANCH="debian/sid"
+fi
+git clone -b ${BRANCH} https://github.com/openwebrx/dream-debian
+wget -O dream_2.1.1.orig.tar.gz https://downloads.sourceforge.net/project/drm/dream/2.1.1/dream-2.1.1-svn808.tar.gz
+pushd dream-debian
+tar xvfz ../dream_2.1.1.orig.tar.gz --strip-components=1
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+  GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  gbp dch ${GBP_ARGS}
+  git add debian/changelog
+  git commit -m "snapshot changelog"
+fi
+debuild -us -uc
+popd
