93cbc04a7245e3b52116e78754b953c393024196
Jakob Ketterl <jakob.ketterl@gmx.de>
2023-10-30 14:06:26 +0100

    add codec2 build

---
+++ packages/15-pkg-codec2/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+    source /etc/os-release
+    BRANCH="${ID}/${VERSION_CODENAME}"
+else
+    BRANCH="debian/sid"
+fi
+git clone -b ${BRANCH} https://github.com/openwebrx/pkg-codec2
+pushd pkg-codec2
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+  GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  gbp dch ${GBP_ARGS}
+  git add debian/changelog
+  git commit -m "snapshot changelog"
+fi
+gbp buildpackage --git-debian-branch=${BRANCH} -us -uc
+popd
