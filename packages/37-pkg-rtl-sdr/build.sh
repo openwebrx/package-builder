0b642d1d76b47f05cbc6f109dd675a3e2379d647
Jakob Ketterl <jakob.ketterl@gmx.de>
2023-12-29 23:02:55 +0100

    update rtl-sdr build

--- packages/37-pkg-rtl-sdr/build.sh
+++ packages/37-pkg-rtl-sdr/build.sh
@@ -8,13 +8,9 @@ else
     BRANCH="debian/sid"
 fi
 git clone -b ${BRANCH} https://github.com/openwebrx/pkg-rtl-sdr
-git clone git://git.osmocom.org/rtl-sdr
-pushd rtl-sdr
-# need to create our own tarball from git since the ones available for download don't match the packaging instructions
-git archive --format tar.gz -o ../rtl-sdr_0.6.0.orig.tar.gz 0.6.0
-popd
+wget -O rtl-sdr_2.0.1.orig.tar.gz https://gitea.osmocom.org/sdr/rtl-sdr/archive/v2.0.1.tar.gz
 pushd pkg-rtl-sdr
-tar xvfz ../rtl-sdr_0.6.0.orig.tar.gz --exclude=debian
+tar xvfz ../rtl-sdr_2.0.1.orig.tar.gz --exclude=debian
 if [[ ! -z ${BUILD_NUMBER:-} ]]; then
   GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
   gbp dch ${GBP_ARGS}
