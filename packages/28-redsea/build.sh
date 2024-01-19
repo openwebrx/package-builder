77da9f366ef95c3835cc149df1f79116d4e930a6
Jakob Ketterl <jakob.ketterl@gmx.de>
2024-01-19 01:31:15 +0100

    add redsea build

---
+++ packages/28-redsea/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+    source /etc/os-release
+    BRANCH="${ID}/${VERSION_CODENAME}"
+else
+    BRANCH="debian/sid"
+fi
+git clone -b ${BRANCH} https://github.com/openwebrx/redsea-debian
+pushd resea-debian
+if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+  GBP_ARGS="--debian-branch=${BRANCH} --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  gbp dch ${GBP_ARGS}
+  git add debian/changelog
+  git commit -m "snapshot changelog"
+fi
+gbp buildpackage --git-debian-branch=${BRANCH} -us -uc
+popd
